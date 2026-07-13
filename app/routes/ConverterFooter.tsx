2a7391a10c3770b783a877afce288ff92691f413
somaia02 <somaia0246@gmail.com>
2026-07-13 16:37:16 +0300

    feat: Show indicator when a conversion is logged successfully

--- app/routes/ConverterFooter.tsx
+++ app/routes/ConverterFooter.tsx
@@ -2,6 +2,8 @@ import { type Key } from "react-aria-components";
 import { useFetcher, useLoaderData } from "react-router";
 import "./ConverterFooter.css";
 import FavoriteBtn from "./FavoriteBtn.tsx";
+import { Check } from "./icons.tsx";
+import { useEffect, useState } from "react";
 
 interface ConverterFooterProps {
   info: string;
@@ -18,9 +20,16 @@ export default function ConverterFooter({
   sendValue,
   receiveValue,
 }: ConverterFooterProps) {
+  const [logBtnClicked, setLogBtnClicked] = useState(false);
   let fetcher = useFetcher();
   const data = useLoaderData();
   let conversionLogs = data.conversionLogs;
+  const clickedLogBtn = (
+    <>
+      <Check />
+      <span>Logged</span>
+    </>
+  );
 
   function handleLogClick() {
     const logInfo = {
@@ -38,7 +47,15 @@ export default function ConverterFooter({
         encType: "application/json",
       },
     );
+    setLogBtnClicked(true);
   }
+  useEffect(() => {
+    let timeoutId: string | number | NodeJS.Timeout | undefined;
+    if (logBtnClicked) {
+      timeoutId = setTimeout(() => setLogBtnClicked(false), 2000);
+    }
+    return () => clearTimeout(timeoutId);
+  }, [logBtnClicked]);
   return (
     <div className="converter__footer">
       <p className="converter__info">{info}</p>
@@ -51,10 +68,10 @@ export default function ConverterFooter({
         <button
           type="button"
           onClick={handleLogClick}
-          className="converter__log-btn"
+          className={`converter__log-btn ${logBtnClicked ? "converter__log-btn--logged" : ""}`}
           disabled={!sendValue || !receiveValue}
         >
-          Log conversion
+          {logBtnClicked ? clickedLogBtn : "Log conversion"}
         </button>
       </div>
     </div>
